f91e906b604c4bc0a9889ecad9c4c96c505eb3df
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-10-20 13:20:47 -0700

    Add files via upload

---
+++ ex7.rb
@@ -0,0 +1,6 @@
+#What does the following error message tell you?
+
+#(SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  #from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>')
+
+#There is an opening bracket somewhere where you may have forgotten to close.
