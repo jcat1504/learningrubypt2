2230ed9fa909fbf0fa231668b73093cbaa6e81f3
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-10-25 12:26:52 -0700

    Add files via upload

---
+++ ex13.rb
@@ -0,0 +1,16 @@
+#What do the following expressions evaluate to?
+
+x = 2
+  #=> x = 2
+
+puts x = 2
+  #=> nil
+
+p name = "Joe"
+  #=> Joe
+
+four = "four"
+  #=> "four"
+
+print something = nothing
+  #=> nil
