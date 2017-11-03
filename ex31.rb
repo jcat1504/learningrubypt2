86babc812780d855469ac9cd0a0cbd6487b26e3b
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-11-02 21:08:41 -0700

    Add files via upload

---
+++ ex31.rb
@@ -0,0 +1,10 @@
+#You run the following code...
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names['3'] = 'jody'
+
+#...and get the following error message:
+#TypeError: no implicit conversion of String into Integer
+#what is the problem and how can it be fixed?
+#You are trying to set jody to margaret stating margaret's name but you
+#have to use an INTEGER to do this
