28cc4d2b69633ffb8c61bcae88b2e86786fd1e14
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-11-11 17:12:30 -0800

    Add files via upload

---
+++ ex37.rb
@@ -0,0 +1,10 @@
+classes = {'black mage' => 'Vivi',
+           'white mage' => 'Yuna',
+           'dragoon' => 'Kain',
+           'thief' => 'Zidane'
+         }
+if classes.has_value?('Vivi')
+  puts "Yes it does"
+else
+  puts "Nah"
+end
