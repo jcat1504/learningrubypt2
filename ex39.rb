c51255c02cc3b7c73240f35864128b5491bc4c76
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-11-13 09:52:06 -0800

    Add files via upload

---
+++ ex39.rb
@@ -0,0 +1,9 @@
+#Given the following code...
+
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+#What is the difference between the two hashes that were created?
+# the first hash used x as a key, the second hash used the string
+#"some value" as the key
