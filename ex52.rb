ca23f4b43829d5b28d9e55b5f2ca743b2034e40d
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-11-24 09:41:11 -0800

    Add files via upload

---
+++ ex52.rb
@@ -0,0 +1,4 @@
+#What's the major difference between an Array and a Hash?
+
+#Hashes are meant to store key and value data, and are associated with
+#a certain label. Arrays maintain order in code. 
