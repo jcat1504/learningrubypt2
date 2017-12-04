3051299ebcc0949b7646892179996d53cf980631
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-03 20:51:48 -0800

    Add files via upload

---
+++ ex60.rb
@@ -0,0 +1,12 @@
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+     #turn it into a new array that consists of strings containing
+     #one word. (ex. ["white snow", etc...] → ["white", "snow", etc...].
+     #Look into using Array's map and flatten methods, as well as
+     #String's split method.
+
+a = a.map{|pairs| pairs.split}
+a.flatten
+
+puts a
