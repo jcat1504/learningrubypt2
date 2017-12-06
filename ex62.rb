f663575de0bb09c0ce7b3b5f3f9098436fe53c43
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-06 15:55:31 -0800

    Add files via upload

---
+++ ex62.rb
@@ -0,0 +1,38 @@
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+  
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+
+  def speed_up(number)
+    @speed += number
+    puts "You pushed on the gas by #{number} mph."
+  end
+
+  def brake(number)
+    @speed += number
+    puts "You decelerated by #{number} mph."
+  end
+
+  def shut_off
+    @speed = 0
+    puts "parking..."
+  end
+
+  def speed
+    puts "You are now going #{speed} mph."
+  end
+end
+
+subaru = MyCar.new(2010, 'blue', 'subaru sti')
+subaru.speed_up(10)
+subaru.speed
+subaru.brake(20)
+subaru.speed
+subaru.shut_off
+subaru.speed
