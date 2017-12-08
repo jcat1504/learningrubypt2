4a1257a9d4512200d33743b9e05c81dfceda1bcb
Jess Y <31145424+jcat1504@users.noreply.github.com>
2017-12-08 09:50:58 -0800

    Add files via upload

---
+++ ex64.rb
@@ -0,0 +1,51 @@
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @speed = 0
+  end
+
+  def speed_up(number)
+    @speed += number
+    puts "You pushed on the gas by #{number} mph."
+  end
+
+  def brake(number)
+    @speed += number
+    puts "You decelerated by #{number} mph."
+  end
+
+  def shut_off
+    @speed = 0
+    puts "parking..."
+  end
+
+  def speed
+    puts "You are now going #{speed} mph."
+  end
+
+def spray_paint(color)
+  self.color = color
+  puts "Your car is now #{color}"
+end
+end
+
+
+subaru = MyCar.new(2010, 'blue', 'subaru sti')
+subaru.speed_up(10)
+subaru.speed
+subaru.brake(20)
+subaru.speed
+subaru.shut_off
+subaru.speed
+
+subaru.color = 'blue'
+puts subaru.color
+puts subaru.year
+
+subaru.spray_paint('red')
+puts subaru.spray_paint
